aad764e54fdf8b5cb8a44556501cd6c924c420cc
Andy Novocin <andy@novocin.com>
2021-11-09 22:51:40 -0500

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,14 @@
+<?php
+/*$target_dir = "cakes/";
+$fname = basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . $fname;
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  print_r("We got your cake image: /cakes/$fname");
+  move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+  print_r("<div><img src=\"cakes/$fname\"/></div>");
+}*/
+
+?>
